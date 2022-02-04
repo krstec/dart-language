f8e9f5fbce96e4704a0f5fa71497cffff8862f4b
Rupesh Kumar <97664241+krstec@users.noreply.github.com>
2022-02-04 18:28:44 +0530

    Update f01.dart

--- infosaysClass/f01.dart
+++ infosaysClass/f01.dart
@@ -1,12 +1,13 @@
 //sum of two number add in a program dart
-void main(){
+void main(){  // main function
   print("Sum of two number in the function");
-  //create the function
+  //create the sum function
 
 int Sum(int a,int b){
   int c= a+b;
   return c;
 }
 var c = Sum(20,30);
+  // we are calling c function and out-put program  
 print('sum of two number ${c}');
 }
