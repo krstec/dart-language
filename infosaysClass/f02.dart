2bb33b7f007b106fbfcdf3223bb54e9e22aad7ea
krstec <krs8tec@gmail.com>
2022-02-04 19:32:31 +0530

    Create f02.dart

---
+++ infosaysClass/f02.dart
@@ -0,0 +1,10 @@
+
+void main(){ // main function
+ 
+  //calling the function inside print statement function
+  print(greeting());
+  }
+ String greeting(){
+    //creating a function without argumment 
+    return  'Hello KrsTec';
+  }
