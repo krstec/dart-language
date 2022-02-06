ef1210c67ab8345bd5a9e2d9b1103047078f8af2
krstec <krs8tec@gmail.com>
2022-02-06 10:51:45 +0530

    Update f01.dart

--- infosaysClass/f01.dart
+++ infosaysClass/f01.dart
@@ -1,13 +1,33 @@
-//sum of two number add in a program dart
-void main(){  // main function
-  print("Sum of two number in the function");
-  //create the sum function
+// //sum of two number add in a program dart
+// void main(){  // main function
+//   print("Sum of two number in the function");
+//   //create the sum function
 
-int Sum(int a,int b){
-  int c= a+b;
-  return c;
+// int Sum(int a,int b){
+//   int c= a+b;
+//   return c;
+// }
+// var c= Sum(20,30);
+//   // we are calling c function and out-put program  
+// print('sum of two number ${c}');
+// }
+
+
+void main(){
+  display('Kamlesh',90);
+  display1(
+  name: "Kamlesh");
 }
-var c = Sum(20,30);
-  // we are calling c function and out-put program  
-print('sum of two number ${c}');
+
+void display([String name='', int age=0]){
+print("object");
+print(name);
+print(age);
+}
+
+
+void display1({String name='',int age=0}){
+  print(name);
+  print(age);
+
 }
