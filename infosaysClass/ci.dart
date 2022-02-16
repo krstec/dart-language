c51cc15e168433ee752abba3b4a3529cb9a10ec0
krstec <krs8tec@gmail.com>
2022-02-16 12:24:38 +0530

    Create ci.dart

---
+++ infosaysClass/ci.dart
@@ -0,0 +1,21 @@
+
+void main(){
+
+st();
+}
+class student{
+  void name(){
+    print("this student $name");
+  }
+  void id(){
+    print('student id is $id');
+
+  }
+}
+class st extends student {
+  void cls() {
+    print('B.tec');
+  }
+}
+
+
